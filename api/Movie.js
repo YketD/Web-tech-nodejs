0f38f2c56d1928588b0e5aed7c5eaa306f70b038
Yke ten Donkelaar <yketd@hotmail.com>
2016-10-04 17:14:34 +0200

    model

---
+++ api/Movie.js
@@ -0,0 +1,4 @@
+/**
+ * Created by yketd on 4-10-2016.
+ */
+var Movie = require('MovieModel.js');
---
+++ api/Rating.js
@@ -0,0 +1,5 @@
+/**
+ * Created by yketd on 4-10-2016.
+ */
+var Rating = require('RatingModel.js');
+
